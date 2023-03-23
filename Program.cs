8fe9be873ec5dac29543b3ffacc54cfe878d0d59
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-03-23 15:00:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using CA230323;
+using System.Text;
+
+List<Versenyzo> versenyzok = new();
+using StreamReader sr = new("..\\..\\..\\src\\Eredmenyek.txt", Encoding.Latin1);
+while(!sr.EndOfStream) versenyzok.Add(new Versenyzo(sr.ReadLine()!));
+
+Console.WriteLine($"a versenyt {versenyzok.Count} versenyző fejezte be");
+
+int noej = 0;
+foreach (var v in versenyzok)
+    if (v.KorKategoria == "elit junior") noej++;
+Console.WriteLine($"a versenyzők száma \"elit junior\" kategóriában: {noej} fő");
+
+int esum = 0;
+foreach (var v in versenyzok)
+    esum += (2014 - v.SzulEv);
+Console.WriteLine($"a versenyzők átlagéletkora: {esum/(float)versenyzok.Count:0.0} év");
+
+Console.Write("írd be egy kategória nevét: ");
+string kk = Console.ReadLine()!;
+string rszok = string.Empty;
+foreach (var v in versenyzok)
+    if (v.KorKategoria == kk) rszok += $"{v.RajtSzam} ";
+if (rszok == string.Empty)
+    Console.WriteLine("\tnincs ilyen kategória!");
+else Console.WriteLine($"\trajtszám(ok): {rszok}");
+
+List<Versenyzo> nok = new();
+foreach (var v in versenyzok) if (!v.Nem) nok.Add(v);
+
+int mini = 0;
+for (int i = 1; i < nok.Count; i++)
+    if (nok[i].Osszido < nok[mini].Osszido) mini = i;
+Console.WriteLine($"a nők között a nyőztes: {nok[mini].Nev}");
---
+++ Versenyzo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA230323
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int SzulEv { get; set; }
+        public string RajtSzam { get; set; }
+        public bool Nem { get; set; }
+        public string KorKategoria { get; set; }
+        public TimeSpan[] IdoEredmenyek { get; set; }
+
+        public TimeSpan Osszido
+        {
+            get
+            {
+                TimeSpan oi = TimeSpan.Zero;
+                foreach (var ie in IdoEredmenyek) oi += ie;
+                return oi;
+            }
+        }
+
+        public Versenyzo(string sor)
+        {
+            string[] v = sor.Split(';');
+            Nev = v[0];
+            SzulEv = int.Parse(v[1]);
+            RajtSzam = v[2];
+            Nem = v[3] == "f";
+            KorKategoria = v[4];
+            IdoEredmenyek = new TimeSpan[5];
+            for (int i = 5; i < v.Length; i++)
+                IdoEredmenyek[i - 5] = TimeSpan.Parse(v[i]);
+        }
+    }
+}
